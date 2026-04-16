4ee694efe8026895f3d50e1d0623af1d26c91aab
kayque-end <araujokayque75@gmail.com>
2026-04-16 12:25:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,65 +4,29 @@ const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const express = require('express');
+const app = express();
 
 
-const server = http.createServer((req, res) => {
-    const url = decodeURI(req.url)
-    let aquivohtml ;
-    if (url === "/produtoa") {
-        res.statusCode = 200;
-        res.setHeader('Content-Type' , 'text/html; charset=utf-8');
-        aquivohtml = fs.readFileSync("./src/produtoa.html")
-     
-        res.end(aquivohtml)
-    }
-    else if (url === "/"){
-        res.statusCode = 200;
-        res.setHeader('Content-Type' , 'text/html; charset=utf-8');
-        aquivohtml = fs.readFileSync("./src/index.html")
-        res.end(aquivohtml)
-    }
-    else if (url === "/produtob"){
-        res.statusCode = 200;
-        res.setHeader('Content-Type' , 'text/html; charset=utf-8');
-        aquivohtml = fs.readFileSync("./src/produtob.html")
-        res.end(aquivohtml)
-    }
-    else if (url === "/css/index.css"){
-        res.statusCode = 200;
-        res.setHeader('Content-Type' , 'text/css; charset=utf-8');
-        aquivohtml = fs.readFileSync("./css/index.css")
-        res.end(aquivohtml)
-    }
-    else if (url === "/css/produtoa.css"){
-        res.statusCode = 200;
-        res.setHeader('Content-Type' , 'text/css; charset=utf-8');
-        aquivohtml = fs.readFileSync("./css/produtoa.css")
-        res.end(aquivohtml)
-    }
-    else if (url === "/css/produtob.css"){
-        res.statusCode = 200;
-        res.setHeader('Content-Type' , 'text/css; charset=utf-8');
-        aquivohtml = fs.readFileSync("./css/produtob.css")
-        res.end(aquivohtml)
-    }
-    else if (url === "/imagens/download (20).jpg"){
-        res.statusCode = 200;
-        res.setHeader('Content-Type' , 'image/jpeg');
-        aquivohtml = fs.readFileSync("./css/imagens/download (20).jpg")
-        res.end(aquivohtml)
-    }
-   
-    else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type' , 'text/html; charset=utf-8');
-        res.end("Pagina nao encontrada");
-    }
-})
-           
- 
-            
+// arquivos estáticos
+app.use(express.static(__dirname + '/public'));// dirname é o caminho da pasta do projeto
 
-server.listen(port, hostname, () => {
-    console.log(`Servidor rodando em http://${hostname}:${port}/`);
+//rotaa
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html');
 });
+
+app.get('/produtoA', (req, res) => {
+    res.sendFile(__dirname + '/src/produtoa.html');
+});
+
+app.get('/produtoB', (req, res) => {
+    res.sendFile(__dirname + '/src/produtob.html');
+});
+
+app.listen(port, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+});       
+ 
+
+
